6563ec76159158e30d8a785561626dd77d2c2dcf
Mathieu Hofman <mathieu.hofman@citrix.com>
2016-05-11 17:33:23 -0700

    fix: Normalize a scoped package name when bower.json file doesn't exist

--- src/bower-utils.js
+++ src/bower-utils.js
@@ -76,6 +76,12 @@ module.exports = {
               'repository'
             ]);
 
+            // scoped packages get special treatment
+            // See https://github.com/npm/npm/blob/2a5977e0c65b244e92d848fcd56f2f80ba8cdf3b/lib/pack.js#L53
+            if (newConfig.name[0] === '@') {
+              newConfig.name = newConfig.name.substr(1).replace(/\//g, '-');
+            }
+
             // Do not try to translate dependencies.
             // Maybe be can try to deduce the dependencies ?
             newConfig.dependencies = {};
