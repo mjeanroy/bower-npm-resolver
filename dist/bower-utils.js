f5ccbdc1972816b796b46394873188c95adc1fe1
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2018-02-25 16:08:24 +0100

    release: release version

---
+++ dist/bower-utils.js
@@ -0,0 +1,97 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2018 Mickael Jeanroy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+'use strict';
+
+/**
+ * This module is used as a wrapper for NPM commands.
+ * Each functions will returned a promise:
+ *  - Resolved with the desired result.
+ *  - Rejected with the error returned from NPM.
+ */
+
+var path = require('path');
+var fs = require('fs');
+var Q = require('q');
+var _ = require('lodash');
+
+/**
+ * Check that a given path is a valid file and returns a promise
+ * that is resolved if the path is a file and rejected if not.
+ *
+ * @param {string} path The path to check.
+ * @return {Promise} The promise.
+ */
+function isFile(path) {
+  var deferred = Q.defer();
+
+  fs.stat(path, function (err, stat) {
+    if (err || !stat.isFile()) {
+      deferred.reject();
+    } else {
+      deferred.resolve();
+    }
+  });
+
+  return deferred.promise;
+};
+
+module.exports = {
+  patchConfiguration: function patchConfiguration(pkg) {
+    var deferred = Q.defer();
+    var bowerJson = path.join(pkg, 'bower.json');
+
+    isFile(bowerJson).then(deferred.resolve).catch(function () {
+      fs.readFile(path.join(pkg, 'package.json'), 'utf8', function (err, data) {
+        if (err) {
+          deferred.reject('Could not read package.json from ' + pkg);
+        } else {
+          var config = JSON.parse(data);
+          var newConfig = _.pick(config, ['name', 'description', 'main', 'moduleType', 'keywords', 'authors', 'license', 'ignore', 'private', 'homepage', 'bugs', 'repository']);
+
+          // scoped packages get special treatment
+          // See https://github.com/npm/npm/blob/v3.9.1/lib/pack.js#L53
+          if (newConfig.name[0] === '@') {
+            newConfig.name = newConfig.name.substr(1).replace(/\//g, '-');
+          }
+
+          // Do not try to translate dependencies.
+          // Maybe be can try to deduce the dependencies ?
+          newConfig.dependencies = {};
+          newConfig.devDependencies = {};
+
+          fs.writeFile(bowerJson, JSON.stringify(newConfig, null, 2), 'utf-8', function (err) {
+            if (err) {
+              deferred.reject('Could not write bower.json file to ' + pkg);
+            } else {
+              deferred.resolve();
+            }
+          });
+        }
+      });
+    });
+
+    return deferred.promise;
+  }
+};
---
+++ dist/extract.js
@@ -0,0 +1,71 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2018 Mickael Jeanroy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+'use strict';
+
+/**
+ * This module will extract an NPM package into
+ * a destination directory.
+ * NPM package are `tar.gz` files, so this module will:
+ * - Unzip the package.
+ * - Extract it.
+ */
+
+var fs = require('fs');
+var zlib = require('zlib');
+var tar = require('tar-fs');
+var Q = require('q');
+
+/**
+ * Check that the given file is a symbolic link.
+ *
+ * @param {Object} file The file to check.
+ * @return {boolean} `true` if the file is a symbolic link, `false` otherwise.
+ */
+function isSymlink(file) {
+  return file.type === 'SymbolicLink';
+}
+
+module.exports = {
+  /**
+   * Extract the package.
+   *
+   * This function return a promise resolved with the path
+   * to the extracted file.
+   *
+   * If an error occurred (during unzip, or extraction), the promise
+   * will be rejected with the error object.
+   *
+   * @param {String} src Source file to extract.
+   * @param {String} dst Destination directory.
+   * @return {Promise} The promise.
+   */
+  tgz: function tgz(src, dst) {
+    var deferred = Q.defer();
+
+    fs.createReadStream(src).on('error', deferred.reject).pipe(zlib.createGunzip()).on('error', deferred.reject).pipe(tar.extract(dst, { ignore: isSymlink, dmode: '0555', fmode: '0444' })).on('error', deferred.reject).on('finish', deferred.resolve.bind(deferred, dst));
+
+    return deferred.promise;
+  }
+};
