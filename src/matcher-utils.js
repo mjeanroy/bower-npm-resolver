cebd6087a6b725556cab8cc03c7a58087a61b459
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2018-02-25 15:56:57 +0100

    docs: update license copyright

--- src/matcher-utils.js
+++ src/matcher-utils.js
@@ -1,7 +1,7 @@
 /**
  * The MIT License (MIT)
  *
- * Copyright (c) 2016-2017 Mathieu Hofman
+ * Copyright (c) 2016-2018 Mathieu Hofman, Mickael Jeanroy
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
