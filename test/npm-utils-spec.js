365b529eabc3127b687208afac9a5e04f0073117
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2020-08-21 18:08:40 +0200

    fix(test): require src instead of dist

--- test/npm-utils-spec.js
+++ test/npm-utils-spec.js
@@ -27,7 +27,7 @@
 const fs = require('fs');
 const path = require('path');
 const tmp = require('tmp');
-const npmUtil = require('../dist/npm-utils');
+const npmUtil = require('../src/npm-utils');
 
 describe('npm-utils', () => {
   let tmpDir;
